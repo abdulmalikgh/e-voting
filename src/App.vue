1239c27d88aa84079c841f5bebf5dd6f0ee29bac
Musah Abdul-Malik <musah.abdulmalikgh@gmail.com>
2020-12-22 16:02:57 +0000

    Add sidebar

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,9 @@
 </template>
 
 <style>
-html, body, div{
+html, body{
   width:100%;
-  height: 100%;
   margin:0;
   padding:0;
-}
-#app{
-  width:100%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,8 +25,32 @@
       </div>
     </nav>
     <!-- Hero -->
-    <div class="hero-container">
-
+    <div class="hero-container container-fluid">
+         <!-- <div class="row top-nav-wrapper">
+            <div class="col-lg-10 col-sm-12 col-md-10">
+            <ul class="top-navigation">
+              <li>
+                <a href=""> Core Members</a>
+              </li>
+              <li>
+                <a href="">Auxilliary Members</a>
+              </li>
+            </ul>
+        </div>
+         </div> -->
+    </div>
+    <!-- side navigation -->
+    <div class="container-fluid">
+        <div class="main-container">
+          <div class="side-navigation">
+             <ul>
+               <li><a href="">Presidential</a></li>
+               <li><a href="">Vice President</a></li>
+               <li><a href="">General Secretary</a></li>
+               <li><a href="">Financial Secretary</a></li>
+             </ul>
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -35,7 +59,37 @@
 
 export default {
   name: 'Home',
-
+  data() {
+    return {
+      topNavActive:false,
+      users: [
+        {
+          name:'Adjoa Frimpong',
+          title:'Adjoa the leader',
+          team: 'Blue team',
+          image:require('../assets/imageOne.jpeg')
+        },
+         {
+          name:'Adjoa Frimpong',
+          title:'Adjoa the leader',
+          team: 'Royal team',
+          image:require('../assets/imageTwo.jpeg')
+        },
+         {
+          name:'Adjoa Frimpong',
+          title:'Adjoa the leader',
+          team: 'Margenta team',
+          image:require('../assets/imageThree.jpeg')
+        },
+         {
+          name:'Kofi baboni',
+          title:'Adjoa the leader',
+          team: 'Gold team',
+          image:require('../assets/imageFour.jpeg')
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -69,5 +123,47 @@ export default {
     width:100%;
     background-position: center;
     background-size: cover;
+    display: flex;
+  }
+  .top-navigation{
+      list-style: none;
+  }
+  .top-navigation li {
+    display: inline-block;
+  }
+  .top-navigation ul li {
+    width: 200px !important;
+  }
+  .top-navigation ul li a{
+    text-decoration: none;
+    background-color: #eee;
+    padding:20px 30px;
+    text-align: center;
+    color:#000
+  }
+  .top-nav-wrapper{
+    position:absolute;
+  }
+  /* Main container */
+  .main-container{
+    margin: 50px auto;
+    width:80%;
+  }
+
+  /* side navigation  */
+  .side-navigation ul {
+    list-style: none;
+  }
+  .side-navigation ul li {
+    width:200px ;
+    margin:10px 0;
+    padding:10px 20px;
+    background-color: rgb(129, 123, 123);
+    border-radius: 10px ;
+    cursor: pointer;
+  }
+  .side-navigation ul li a{
+    color:#fff;
+    text-decoration: none;
   }
 </style>
